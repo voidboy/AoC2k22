a42dbfb261ba1c710b65fb110849fafcba2bd9a8
voidboy <aurelienclerac@pm.me>
2022-12-02 09:31:53 +0100

    lets get rusty

---
+++ day2/day2_part2.rs
@@ -0,0 +1,27 @@
+use std::fs;
+
+/* Thanks Mr. russriguez */
+
+fn compute_score(contents: &String, choice: bool) -> u32 {
+    contents.lines().map(|line|{
+        match line {
+            "A X" => (4,3), 
+            "A Y" => (8,4),
+            "A Z" => (3,8),
+            "B X" => (1,1),
+            "B Y" => (5,5),
+            "B Z" => (9,9),
+            "C X" => (7,2),
+            "C Y" => (2,6),
+            "C Z" => (6,7),
+            _ => panic!("invalid line !"),
+        }
+    }).map(|(a, b)| if !choice { a } else { b } ).sum()
+}
+
+fn main() {
+    let contents = fs::read_to_string("input")
+        .expect("Cannot read the file");
+    println!("Part. 1 is {}", compute_score(&contents, false));
+    println!("Part. 2 is {}", compute_score(&contents, true));
+}
