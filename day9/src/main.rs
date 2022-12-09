26ec5d285445f22be02a83a1fee73afc3dc8f438
voidboy <aurelienclerac@pm.me>
2022-12-09 10:22:37 +0100

    refactor

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -29,39 +29,49 @@ Part. I: 6339
 
 */
 
-fn fix_up_down(knots: &mut Vec<(isize, isize)>, i: usize, visit: &mut HashSet<(isize, isize)>) {
-    if knots[i - 1].1.abs_diff(knots[i].1) == 2 {
-        if knots[i - 1].1 > knots[i].1 {
-            knots[i].1 += 1;
-        } else {
-            knots[i].1 -= 1;
-        }
-        knots[i].0 = knots[i - 1].0;
-        if i == knots.len() - 1 {
-            visit.insert(knots[i]);
+type Pos = (isize, isize);
+
+fn fix_up_down(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
+    for i in 1..knots.len() {
+        if knots[i - 1].1.abs_diff(knots[i].1) == 2 {
+            if knots[i - 1].1 > knots[i].1 {
+                // UP
+                knots[i].1 += 1;
+            } else {
+                // DOWN
+                knots[i].1 -= 1;
+            }
+            knots[i].0 = knots[i - 1].0;
+            if i == knots.len() - 1 {
+                visit.insert(knots[i]);
+            }
         }
     }
 }
 
-fn fix_left_right(knots: &mut Vec<(isize, isize)>, i: usize, visit: &mut HashSet<(isize, isize)>) {
-    if knots[i - 1].0.abs_diff(knots[i].0) == 2 {
-        if knots[i - 1].0 > knots[i].0 {
-            knots[i].0 += 1;
-        } else {
-            knots[i].0 -= 1;
-        }
-        knots[i].1 = knots[i - 1].1;
-        if i == knots.len() - 1 {
-            visit.insert(knots[i]);
+fn fix_left_right(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
+    for i in 1..knots.len() {
+        if knots[i - 1].0.abs_diff(knots[i].0) == 2 {
+            if knots[i - 1].0 > knots[i].0 {
+                // RIGHT
+                knots[i].0 += 1;
+            } else {
+                // LEFT
+                knots[i].0 -= 1;
+            }
+            knots[i].1 = knots[i - 1].1;
+            if i == knots.len() - 1 {
+                visit.insert(knots[i]);
+            }
         }
     }
 }
 
-fn main() {
+fn ropes_simulation(nbr_of_knots: usize) -> usize {
     let contents = fs::read_to_string("input").expect("Cannot read the input file");
 
-    let mut knots: Vec<(isize, isize)> = vec![(0, 0); 2];
-    let mut visit: HashSet<(isize, isize)> = HashSet::new();
+    let mut knots: Vec<Pos> = vec![(0, 0); nbr_of_knots];
+    let mut visit: HashSet<Pos> = HashSet::new();
     for line in contents.lines() {
         let mut motion = line.split_whitespace();
         let (direction, length) = (
@@ -72,33 +82,25 @@ fn main() {
             "U" => {
                 for _ in 0..length {
                     knots[0].1 += 1;
-                    for i in 1..knots.len() {
-                        fix_up_down(&mut knots, i, &mut visit);
-                    }
+                    fix_up_down(&mut knots, &mut visit);
                 }
             }
             "D" => {
                 for _ in 0..length {
                     knots[0].1 -= 1;
-                    for i in 1..knots.len() {
-                        fix_up_down(&mut knots, i, &mut visit);
-                    }
+                    fix_up_down(&mut knots, &mut visit);
                 }
             }
             "R" => {
                 for _ in 0..length {
                     knots[0].0 += 1;
-                    for i in 1..knots.len() {
-                        fix_left_right(&mut knots, i, &mut visit);
-                    }
+                    fix_left_right(&mut knots, &mut visit);
                 }
             }
             "L" => {
                 for _ in 0..length {
                     knots[0].0 -= 1;
-                    for i in 1..knots.len() {
-                        fix_left_right(&mut knots, i, &mut visit);
-                    }
+                    fix_left_right(&mut knots, &mut visit);
                 }
             }
             _ => {
@@ -106,5 +108,10 @@ fn main() {
             }
         }
     }
-    println!("Part. I: {}", visit.len());
+    visit.len()
+}
+
+fn main() {
+    println!("Part. I:  {}", ropes_simulation(2));
+    println!("Part. II: {}", ropes_simulation(10));
 }
