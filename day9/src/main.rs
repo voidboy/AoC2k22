7143663365b1cd809384bf231f744dbd1a4b2327
voidboy <aurelienclerac@pm.me>
2022-12-09 10:54:51 +0100

    still to low

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -31,7 +31,7 @@ Part. I: 6339
 
 type Pos = (isize, isize);
 
-fn fix_up_down(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
+fn fix_knots(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
     for i in 1..knots.len() {
         if knots[i - 1].1.abs_diff(knots[i].1) == 2 {
             if knots[i - 1].1 > knots[i].1 {
@@ -42,15 +42,7 @@ fn fix_up_down(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
                 knots[i].1 -= 1;
             }
             knots[i].0 = knots[i - 1].0;
-            if i == knots.len() - 1 {
-                visit.insert(knots[i]);
-            }
         }
-    }
-}
-
-fn fix_left_right(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
-    for i in 1..knots.len() {
         if knots[i - 1].0.abs_diff(knots[i].0) == 2 {
             if knots[i - 1].0 > knots[i].0 {
                 // RIGHT
@@ -60,9 +52,9 @@ fn fix_left_right(knots: &mut Vec<Pos>, visit: &mut HashSet<Pos>) {
                 knots[i].0 -= 1;
             }
             knots[i].1 = knots[i - 1].1;
-            if i == knots.len() - 1 {
-                visit.insert(knots[i]);
-            }
+        }
+        if i == knots.len() - 1 {
+            visit.insert(knots[i]);
         }
     }
 }
@@ -71,7 +63,7 @@ fn ropes_simulation(nbr_of_knots: usize) -> usize {
     let contents = fs::read_to_string("input").expect("Cannot read the input file");
 
     let mut knots: Vec<Pos> = vec![(0, 0); nbr_of_knots];
-    let mut visit: HashSet<Pos> = HashSet::new();
+    let mut visit: HashSet<Pos> = HashSet::from([(0, 0)]);
     for line in contents.lines() {
         let mut motion = line.split_whitespace();
         let (direction, length) = (
@@ -82,25 +74,25 @@ fn ropes_simulation(nbr_of_knots: usize) -> usize {
             "U" => {
                 for _ in 0..length {
                     knots[0].1 += 1;
-                    fix_up_down(&mut knots, &mut visit);
+                    fix_knots(&mut knots, &mut visit);
                 }
             }
             "D" => {
                 for _ in 0..length {
                     knots[0].1 -= 1;
-                    fix_up_down(&mut knots, &mut visit);
+                    fix_knots(&mut knots, &mut visit);
                 }
             }
             "R" => {
                 for _ in 0..length {
                     knots[0].0 += 1;
-                    fix_left_right(&mut knots, &mut visit);
+                    fix_knots(&mut knots, &mut visit);
                 }
             }
             "L" => {
                 for _ in 0..length {
                     knots[0].0 -= 1;
-                    fix_left_right(&mut knots, &mut visit);
+                    fix_knots(&mut knots, &mut visit);
                 }
             }
             _ => {
