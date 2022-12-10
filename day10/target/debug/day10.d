8f20c39b2c9b18a1f3f82eb687c02bd0ea73cb98
voidboy <aurelienclerac@pm.me>
2022-12-10 09:33:56 +0100

    day10, part1

--- day10/target/debug/day10.d
+++
@@ -1,1 +0,0 @@
-/home/voidboy/fun/AoC2k22/day10/target/debug/day10: /home/voidboy/fun/AoC2k22/day10/src/main.rs
--- day10/target/debug/deps/day10-34d91929b6c72f84.d
+++
@@ -1,5 +0,0 @@
-/home/voidboy/fun/AoC2k22/day10/target/debug/deps/day10-34d91929b6c72f84: src/main.rs
-
-/home/voidboy/fun/AoC2k22/day10/target/debug/deps/day10-34d91929b6c72f84.d: src/main.rs
-
-src/main.rs:
